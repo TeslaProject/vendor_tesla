f7ff9fa1f9f4a49ffc052d1854b0ee1839bc8773
theringmaster <gzroms@hotmail.com>
2015-03-22 11:52:42 -0400

    Few Changes
    Change mod version
    Add stock lolliopop bootani

--- configs/common.mk
+++ configs/common.mk
@@ -26,7 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.amr.wideband=1 \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
-    ro.mod.display.version=14-PI \
+    ro.mod.display.version=1.0 \
     drm.service.enabled=true \
      
 # Common overlays
