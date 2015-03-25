9cd6cd751ed0369b3504ee73a6563cba800517a9
Randy DuCharme <radio.ad5gb@gmail.com>
2015-03-25 12:50:08 -0500

    Add hammerhead lunch menu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo tesla_bacon-userdebug
+add_lunch_combo tesla_hammerhead-userdebug
 add_lunch_combo tesla_shamu-userdebug
 add_lunch_combo tesla_vs985-userdebug
 add_lunch_combo tesla_yuga-userdebug
