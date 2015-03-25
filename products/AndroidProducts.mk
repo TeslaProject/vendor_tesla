da2964487d639e698908db4b28b9fa2af71ef873
Adam <ovadia.adam@gmail.com>
2015-03-25 17:56:38 -0400

    add flo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tesla_shamu.mk \
     $(LOCAL_DIR)/tesla_bacon.mk \
+    $(LOCAL_DIR)/tesla_flo.mk \
     $(LOCAL_DIR)/tesla_vs985.mk \
     $(LOCAL_DIR)/tesla_yuga.mk
