a7ee37e5aa2fdc28599f9bfbf40ff57f5ff805f0
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-03-25 21:01:53 -0400

    hopefully this will fix it lol

--- products/tesla_d851.mk
+++ products/tesla_d851.mk
@@ -1,10 +1,28 @@
-PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+#
+# Copyright 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product, device/lge/d851/full_d851.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common tesla stuff.
-$(call inherit-product, vendor/tesla/configs/common.mk)
+# Inherit from d851 device
+$(call inherit-product, device/lge/d851/device.mk)
 
-PRODUCT_NAME := tesla_d851
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := d851
+PRODUCT_NAME := full_d851
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D851
+PRODUCT_MANUFACTURER := LGE
