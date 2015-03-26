3375c87887c16d5da9376e2eb8bc1c0646137ad0
Alx31 <alex31.couderc@gmail.com>
2015-03-26 10:08:21 +0100

    Alphabetisation

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,10 +1,10 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/tesla_shamu.mk \
     $(LOCAL_DIR)/tesla_bacon.mk \
     $(LOCAL_DIR)/tesla_d851.mk \
     $(LOCAL_DIR)/tesla_d852.mk \
     $(LOCAL_DIR)/tesla_flo.mk \
+    $(LOCAL_DIR)/tesla_jfltetmo.mk \
     $(LOCAL_DIR)/tesla_m8.mk \
+    $(LOCAL_DIR)/tesla_shamu.mk \
     $(LOCAL_DIR)/tesla_vs985.mk \
-    $(LOCAL_DIR)/tesla_yuga.mk \
-    $(LOCAL_DIR)/tesla_jfltetmo.mk
+    $(LOCAL_DIR)/tesla_yuga.mk
