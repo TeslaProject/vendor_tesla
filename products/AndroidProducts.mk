3b9e9fce81a5ee835c9cb3c37b816397c8a8f44b
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-03-27 16:24:07 -0400

    add m8

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tesla_d852.mk \
     $(LOCAL_DIR)/tesla_flo.mk \
     $(LOCAL_DIR)/tesla_jfltetmo.mk \
+    $(LOCAL_DIR)/tesla_m8.mk \
     $(LOCAL_DIR)/tesla_shamu.mk \
     $(LOCAL_DIR)/tesla_vs985.mk \
     $(LOCAL_DIR)/tesla_yuga.mk
