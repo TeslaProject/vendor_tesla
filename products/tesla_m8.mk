daf188d0e505d6402d4e82f1e8735ba069ba4524
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-03-27 15:58:56 -0400

    Update tesla_m8.mk

--- products/tesla_m8.mk
+++ products/tesla_m8.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 $(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common.mk)
 
 PRODUCT_NAME := tesla_m8
