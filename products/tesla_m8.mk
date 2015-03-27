17c36df669642920814e8ff62564211d6d3fe2bb
chris4824 <chris482422@gmail.com>
2015-03-27 16:33:26 -0400

    Update tesla_m8.mk

--- products/tesla_m8.mk
+++ products/tesla_m8.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common.mk)
+$(call inherit-product, vendor/tesla/configs/common.mk)
 
 PRODUCT_NAME := tesla_m8
