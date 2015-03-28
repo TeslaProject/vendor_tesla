68f4b470eff2ae964bac3116861765d2c1354d84
oadam11 <ovadia.adam@gmail.com>
2015-03-28 17:42:15 -0400

    add d855 	modified:   products/AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,6 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tesla_d850.mk \
     $(LOCAL_DIR)/tesla_d851.mk \
     $(LOCAL_DIR)/tesla_d852.mk \
+    $(LOCAL_DIR)/tesla_d855.mk \
     $(LOCAL_DIR)/tesla_flo.mk \
     $(LOCAL_DIR)/tesla_jfltetmo.mk \
     $(LOCAL_DIR)/tesla_m8.mk \
