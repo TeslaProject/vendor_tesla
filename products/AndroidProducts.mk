3d9425740ac66c8ab04c9016b4c617b5cd4aa8e1
Ground Zero Roms Prelim <darksymmetry@outlook.com>
2015-03-28 20:33:01 -0400

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,6 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tesla_flo.mk \
     $(LOCAL_DIR)/tesla_jfltetmo.mk \
     $(LOCAL_DIR)/tesla_m8.mk \
+    $(LOCAL_DIR)/tesla_mako.mk \
     $(LOCAL_DIR)/tesla_shamu.mk \
     $(LOCAL_DIR)/tesla_vs985.mk \
     $(LOCAL_DIR)/tesla_yuga.mk \
