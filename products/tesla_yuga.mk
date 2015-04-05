b22811304fb8ebbb38266bf969eafb7e10bce34e
Alx31 <alex31.couderc@gmail.com>
2015-04-05 22:23:55 +0200

    aosp style needed

--- products/tesla_yuga.mk
+++ products/tesla_yuga.mk
@@ -28,7 +28,13 @@ $(call inherit-product, device/sony/yuga/full_yuga.mk)
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/tesla/configs/common.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys"
-
 PRODUCT_NAME := tesla_yuga
 PRODUCT_DEVICE := yuga
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia Z
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+		PRODUCT_NAME=C6603 \
+		BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys \
+		PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys" \
