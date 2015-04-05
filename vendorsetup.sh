9d82919e678380d20f1a458bd2061902482c93ed
Alx31 <alex31.couderc@gmail.com>
2015-04-05 21:18:42 +0200

    Alphabetization to keep it clean =)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,10 @@ add_lunch_combo tesla_d855-userdebug
 add_lunch_combo tesla_flo-userdebug
 add_lunch_combo tesla_grouper-userdebug
 add_lunch_combo tesla_hammerhead-userdebug
+add_lunch_combo tesla_jfltetmo-userdebug
 add_lunch_combo tesla_ls990-userdebug
 add_lunch_combo tesla_m8-userdebug
 add_lunch_combo tesla_mako-userdebug
 add_lunch_combo tesla_shamu-userdebug
 add_lunch_combo tesla_vs985-userdebug
 add_lunch_combo tesla_yuga-userdebug
-add_lunch_combo tesla_jfltetmo-userdebug
